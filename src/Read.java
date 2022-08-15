c0d2dcbb062f9710f7398b40d6f9a3df92f0f361
Hallcoder <hallcoder25@gmail.com>
2022-08-15 10:39:50 +0200

    grafting in the best

--- src/Read.java
+++ src/Read.java
@@ -1,11 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Read {
     public static void main(String[] args){
         try{
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formattedDate =  DateTimeFormatter.ofPattern("E, MMM dd yyy");
+            System.out.println("Before we start just know that we are:"+now);
+            System.out.println("But the date looks better when formatted "+ now.format(formattedDate));
             File AlreadyExistingFile = new File("newfile.txt");
             Scanner File = new Scanner(AlreadyExistingFile);
             while(File.hasNextLine()){
@@ -16,6 +23,7 @@ public class Read {
             if(AlreadyExistingFile.delete()) System.out.println("Delete done!");
         }catch(FileNotFoundException f){
             System.out.println("Error occured!");
+            f.printStackTrace();
         }
 
 
